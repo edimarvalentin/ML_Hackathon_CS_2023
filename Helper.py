251e94bdbc749a1b9707ec15e4eb36d17f10cc3f
Edimar Valentín Kery <edimar.valentin@upr.edu>
2023-03-26 08:40:44 -0400

    intermediate challenge

--- Helper.py
+++ Helper.py
@@ -1,12 +1,8 @@
 import matplotlib.pyplot as plt
-from IPython import display
 
 plt.ion()
 
 def plot(scores, mean_scores):
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
-    plt.clf()
     plt.title('Training...')
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
@@ -17,3 +13,4 @@ def plot(scores, mean_scores):
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
     plt.show(block=False)
     plt.pause(.1)
+    plt.close()
--- SpaceGame.py
+++ SpaceGame.py
@@ -4,9 +4,6 @@ import pyglet
 import math
 import numpy as np
 
-from Helper import plot
-from Agent import Agent
-
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SPACESHIP_SPEED = 0.01
@@ -20,7 +17,6 @@ class Spaceship:
         self.velocity_x = 0
         self.velocity_y = 0
         self.angle = 0
-        self.pulling_planet = None
 
     def draw(self):
         arcade.draw_rectangle_filled(
@@ -33,23 +29,10 @@ class Spaceship:
         self.y += self.velocity_y
 
     def apply_gravity(self, planet):
-        # Gravity of planet
         dx = planet.x - self.x
         dy = planet.y - self.y
         distance = math.sqrt(dx**2 + dy**2)
         force = GRAVITATIONAL_FORCE * planet.size / distance**2
-
-        # Gravity of current strongest planet
-        if self.pulling_planet is not None:
-            d2x = self.pulling_planet.x - self.x
-            d2y = self.pulling_planet.y - self.y
-            distance = math.sqrt(d2x**2 + d2y**2)
-            old_force = GRAVITATIONAL_FORCE * self.pulling_planet.size / distance**2
-            if force > old_force:
-                self.pulling_planet = planet
-        else:
-            self.pulling_planet = planet
-
         angle = math.degrees(math.atan2(dy, dx))
         self.move(force, angle)
 
@@ -58,7 +41,6 @@ class Spaceship:
         dy = planet.y - self.y
         distance = math.sqrt(dx**2 + dy**2)
         return distance <= (planet.size + 15 + (20 if planet.is_green else 0))
-
 
 
 class Planet:
@@ -77,9 +59,10 @@ class Planet:
 
 class Game(arcade.Window):
 
-    def __init__(self, title):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, title)
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
         self.is_running = True
+
         self.push_handlers(on_key_press=self.on_key_press)
         # print(self.get_event_handlers)
 
@@ -104,14 +87,13 @@ class Game(arcade.Window):
                                  arcade.color.WHITE, font_size=50, anchor_x="center")
                 print("you won!!!")
                 self.is_running = False
-                # reward done, score
-                return (100, True, 1)
+                return (100, True)
             self.spaceship.apply_gravity(planet)
         if self.spaceship.x < 0 or self.spaceship.x > SCREEN_WIDTH or self.spaceship.y < 0 or self.spaceship.y > SCREEN_HEIGHT:
             self.is_running = False
-            return (-100, True, 0)
+            return (-100, True)
         self.spaceship.move(SPACESHIP_SPEED, self.spaceship.angle)
-        return (0, False, 0)
+        return (0, False)
 
     def on_draw(self):
         if self.is_running:
@@ -132,72 +114,33 @@ class Game(arcade.Window):
         print("Key pressed:", key)
 
     def action(self, action):
-        if np.array_equal(action, [1, 0, 0, 0]):  # up
-            self.spaceship.angle = 90
-        elif np.array_equal(action, [0, 1, 0, 0]):  # down
-            self.spaceship.angle = 270
-        elif np.array_equal(action, [0, 0, 1, 0]):  # left
+        if action == 0:
             self.spaceship.angle = 180
-        elif np.array_equal(action, [0, 0, 0, 1]):  # right
+        elif action == 1:
             self.spaceship.angle = 0
+        elif action == 2:
+            self.spaceship.angle = 90
+        elif action == 3:
+            self.spaceship.angle = 270
 
     def run(self):
         # Set up the game loop
-        plot_scores = []
-        plot_mean_scores = []
-        total_score = 0
-        record = 0
-
-        agent = Agent()
-
         while True:
-            # get state of the game
-            state_old = agent.get_state(self)
-
-            # conjure up a move using the forbidden dark arts
-            final_move = agent.get_action(state_old)
-            # get whatever resulted from that move
-            self.action(final_move)
             # Call the update() method with the time since the last frame
-            reward, done, score = self.on_update(1/60)
-            if(done):
-                print(reward)
-            state_new = agent.get_state(self)
-
-            # train the short memory
-            agent.train_short_memory(
-                state_old, final_move, reward, state_new, done)
-
-            # remember
-            agent.remember(state_old, final_move, reward, state_new, done)
-
-            if done:
-                # train the long memory
-                self.reset()
-                agent.n_games += 1
-
-                if score > record:
-                    record = score
-                    agent.model.save()
-
-                plot_scores.append(score)
-                total_score += score
-                mean_score = total_score / agent.n_games
-                plot_mean_scores.append(mean_score)
-                plot(plot_scores, plot_mean_scores)
+            self.on_update(1/60)
 
             if self.is_running:
 
-                    # Clear the screen
+                # Clear the screen
                 arcade.start_render()
 
-                    # Call the on_draw() method to draw the game objects
+                # Call the on_draw() method to draw the game objects
                 self.on_draw()
 
-                    # Swap buffers to display the rendered frame
+                # Swap buffers to display the rendered frame
                 arcade.finish_render()
 
-                    # Sleep to enforce the desired frame rate
+                # Sleep to enforce the desired frame rate
                 arcade.pause(1/60)
             else:
                 self.close()
@@ -225,19 +168,11 @@ class Game(arcade.Window):
         arr = arr.reshape(buffer.height, buffer.width, 4)
         return arr[::-1]
 
-    def get_spaceship(self):
-        return self.spaceship
-
-    def get_screen_width(self):
-        return SCREEN_WIDTH
-
-    def get_screen_height(self):
-        return SCREEN_HEIGHT
-
 
 def main():
-    game = Game('Spaceship Game')
+    game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Spaceship Game")
     game.setup()
+    # arcade.run()
     game.run()
 
 
